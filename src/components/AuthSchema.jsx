f14fc35284a44fa5eda79e6d5be45f5066443f9d
elizabeth <elizabethclarap@gmail.com>
2024-11-14 23:24:17 +0700

    numpang push prev changes

--- src/components/AuthSchema.jsx
+++ src/components/AuthSchema.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigate } from 'react-router-dom';
 
 const signUpFormSchema = z.object({
   name: z
